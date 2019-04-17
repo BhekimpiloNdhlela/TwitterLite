ab0a8a4a99a86e5c71113b89066f737fb8fd0d3c
Bhekimpilo Ndhlela <18998712@sun.ac.za>
2019-04-18 00:58:07 +0200

    add a basic project structure

---
+++ app/utils.py
@@ -0,0 +1,44 @@
+import re
+
+def validatePassword(formInput):
+    '''
+    '''
+    pass
+
+def validateDate(formInput):
+    '''
+    '''
+    pass
+
+def validateName(formInput):
+    '''
+    '''
+    pass
+
+def validateSurname(formInput):
+    '''
+    '''
+    pass
+
+def processPicture(formInput):
+    '''
+    '''
+    pass
+
+def comparePassword():
+    '''
+    '''
+    pass
+
+
+def updatePassword():
+    '''
+    '''
+    pass
+
+def passwordHash():
+    '''
+    '''
+    pass
+
+
