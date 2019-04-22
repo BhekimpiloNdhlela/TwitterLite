4fac44410ae76ee0f28cdd894b9e4b44e5df8a94
Bhekimpilo Ndhlela <18998712@sun.ac.za>
2019-04-23 01:23:23 +0200

    finish get_time_stamp(), get_time(), get_password_hash() in utils.py

--- app/utils.py
+++ app/utils.py
@@ -7,47 +7,7 @@ from passlib.hash import sha512_crypt
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from itsdangerous import URLSafeTimedSerializer
-
-def validate_password(formInput, debug=True):
-    """ uses regular expression to validate password strength """
-    return True
-
-def validate_date(formInput, debug=True):
-    """ uses regular expression to validate password strength """
-    return True
-5
-def validate_name(formInput, debug=True):
-    """ uses regular expression to validate password strength """
-    return True
-
-def validate_surname(formInput, debug=True):
-    """ sumary_line """
-    return True
-
-def process_picture(formInput, debug=True):
-    """ sumary_line """
-    return True
-
-def compare_password(this, that, debug=True):
-    """ sumary_line """
-    return True
-
-
-def update_password(newpassword, oldpassword, debug=True):
-    """ sumary_line """
-    return True
-
-def password_hash(password, debug=True):
-    """ sumary_line """
-    return True
-
-def get_date():
-    """ sumary_line """
-    return None
-
-def set_time_stamp():
-    """ sumary_line """
-    return None
+from datetime import date, datetime
 
 def send_resset_password_email(to_email, from_email='resetpassword@bootlegtwitter.com'):
     """
@@ -57,6 +17,7 @@ def send_resset_password_email(to_email, from_email='resetpassword@bootlegtwitte
     html_content += '<strong>The password for the following account has been changed</strong><br>'
     html_content += '<p>Account: {}</p>'.format(to_email)
     __send_email(from_email, to_email, subject, html_content)
+
 
 def send_forgot_password_email(to_email, token, from_email='forgotpassword@bootlegtwitter.com'):
     """
@@ -84,14 +45,11 @@ def send_account_verification_email(to_email, token, from_email='verifyaccount@b
     __send_message(from_email, to_email, subject, html_content)
 
 
-
 def __send_email(from_email, to_email, subject, html_content):
     """
     [private/ helper] function to send emails, to the user at hand. This helper function can be used
     to send account verrification, password reset and forgot password emails.
-
     NOTE: this function is never called directly hence the {__send_email} notation.
-
     """
     msg = Mail(from_email=from_email, to_emails=to_email, subject=subject, html_content=html_content)
     try:
@@ -99,3 +57,62 @@ def __send_email(from_email, to_email, subject, html_content):
         response = sg.send(msg)
     except Exception as e:
         print(e.message)
+
+
+def get_date():
+    """
+    used to get todays date, this is done when a user is creating a post or when the user is creating
+    an account. Essentially this function is used for anything that needs today's date.
+    """
+    todays_date = date.today().strftime("%Y-%m-%d")
+    return todays_date
+
+
+def get_time_stamp():
+    """
+    used to get the now's time stamp, this is done when a user is creating a post or when the user is
+    creating an account. Essentially this function is used for anything that needs a now's timestamp.
+    """
+    time_stamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    return time_stamp
+
+
+def get_password_hash(password, salt='THESALTISsaltyLi', rounds=99999):
+    """ sumary_line """
+    hash = sha512_crypt.encrypt(password, salt=salt, rounds=rounds)
+    return hash
+
+
+def validate_password(formInput, debug=True):
+    """ uses regular expression to validate password strength """
+    return True
+
+
+def validate_date(formInput, debug=True):
+    """ uses regular expression to validate password strength """
+    return True
+
+
+def validate_name(formInput, debug=True):
+    """ uses regular expression to validate password strength """
+    return True
+
+
+def validate_surname(formInput, debug=True):
+    """ sumary_line """
+    return True
+
+
+def process_picture(form_input, debug=True):
+    """ sumary_line """
+    return True
+
+
+def compare_password(this, that, debug=True):
+    """ sumary_line """
+    return True
+
+
+def update_password(newpassword, oldpassword, debug=True):
+    """ sumary_line """
+    return True
