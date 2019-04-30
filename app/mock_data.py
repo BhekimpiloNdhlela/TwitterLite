1d1b0603092106ee6f89a714932636a988f1e4bc
Keanu <19791615@sun.ac.za>
2019-05-01 01:16:54 +0200

    Add mock data for messages

--- app/mock_data.py
+++ app/mock_data.py
@@ -97,3 +97,118 @@ mock_tweets = [
         "avatar": "/static/img/default.png"
     }
 ]
+
+
+mock_messages = [
+
+    {
+        "username": "jan_doe",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac dapibus orci.",
+        "time": "3 min",
+        "avatar": "/static/img/photos/15.jpg"
+    },
+    {
+        "username": "jan_doe",
+        "content": "Nam in ullamcorper diam, vitae feugiat magna. Mauris a risus ipsum.",
+        "time": "5 min",
+        "avatar": "/static/img/photos/16.jpg"
+    },
+    {
+        "username": "jan_doe",
+        "content": "I don't know",
+        "time": "20 min",
+        "avatar": "/static/img/photos/11.jpg"
+    },
+    {
+        "username": "Bheks",
+        "content": "Suspendisse lobortis aliquet congue. Nulla sagittis nisl massa, vitae gravida velit hendrerit et.",
+        "time": "9 hours",
+        "avatar": "/static/img/default.png"
+    }
+]
+
+mock_followers = [
+
+    {
+        "username": "Fan 1",
+        "avatar": "/static/img/photos/15.jpg"
+    },
+    {
+        "username": "Fan 2",
+        "avatar": "/static/img/photos/16.jpg"
+    },
+    {
+        "username": "Fan 3",
+        "avatar": "/static/img/photos/11.jpg"
+    },
+    {
+        "username": "Bheks",
+        "avatar": "/static/img/default.png"
+    }
+]
+
+mock_following = [
+
+    {
+        "username": "Celeb 1",
+        "avatar": "/static/img/photos/15.jpg"
+    },
+    {
+        "username": "Celeb 2",
+        "avatar": "/static/img/photos/16.jpg"
+    },
+    {
+        "username": "Celeb 3",
+        "avatar": "/static/img/photos/11.jpg"
+    },
+    {
+        "username": "Bheks",
+        "avatar": "/static/img/default.png"
+    }
+]
+
+mock_personal = [
+
+    {
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "photos": [
+            "12.jpg",
+            "3.jpg",
+            "7.jpg"
+        ],
+        "date": "30 April 2019",
+        "retweets": "7",
+        "likes": "10"
+    },
+    {
+        "content": "Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor!",
+        "photos": [
+
+
+        ],
+        "date": "14 March 2019",
+        "retweets": "13",
+        "likes": "40"
+    },
+    {
+        "content": "Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites.",
+        "photos": [
+
+
+        ],
+        "date": "17 December 2018",
+        "retweets": "1",
+        "likes": "9"
+    },
+    {
+        "content": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. ",
+        "photos": [
+
+
+        ],
+        "date": "3 November 2018",
+        "retweets": "43",
+        "likes": "68"
+    },
+    
+]
