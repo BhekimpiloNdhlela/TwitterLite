3d4824f7394a1a95abe13fbd49c2b43c1e4a7534
David Williams <davidpcwilliams27@gmail.com>
2019-05-01 21:48:46 +0200

    fixing session methods

--- app/run.py
+++ app/run.py
@@ -29,7 +29,8 @@ env = Environment(
 def home():
     """ sumary_line """
     template = env.get_template("index.html")
-    user = User(session['username'])
+    user= 'john_dave'
+    #user = User(session['username'])
     return template.render(user=user, tweets=mock_tweets, treading=mock_treading, account=True)
 
 
